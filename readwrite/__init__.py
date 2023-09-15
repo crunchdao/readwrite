33d8d29c67a37822943785208936103e42123dc4
Caceresenzo <caceresenzo1502@gmail.com>
2023-09-15 18:44:03 +0200

    feat(api): skip read/write if path is null

--- readwrite/__init__.py
+++ readwrite/__init__.py
@@ -3,9 +3,16 @@ import typing
 from .registry import get_global_registry
 
 
-def read(path: str, **kwargs):
+def read(path: str, **kwargs) -> typing.Any:
+    if path is None:
+        return None
+    
     return get_global_registry().read(path, **kwargs)
 
 
-def write(x: typing.Any, path: str, **kwargs):
+def write(x: typing.Any, path: str, **kwargs) -> typing.Any:
+    if x is None or path is None:
+        return None
+    
     get_global_registry().write(x, path, **kwargs)
+    return x
