300856d4fa8d1438d3c17a4691dfdc72f4252091
Caceresenzo <caceresenzo1502@gmail.com>
2024-03-17 16:02:29 +0100

    feat(pickle): add `pandas` parameter

--- readwrite/handlers/pickle.py
+++ readwrite/handlers/pickle.py
@@ -10,13 +10,33 @@ class PickleHandler(Handler):
         )
 
     def read(self, path, **kwargs):
-        import pickle
+        use_pandas = kwargs.pop("pandas", False)
 
+        if use_pandas:
+            import pandas
+            return pandas.read_pickle(path, **kwargs)
+
+        import pickle
         with open(path, "rb") as fd:
-            return pickle.load(fd)
+            return pickle.load(fd, **kwargs)
+
+    def read_params(self):
+        return {
+            "pandas": bool,
+        }
 
     def write(self, x, path, **kwargs):
-        import pickle
+        use_pandas = kwargs.pop("pandas", False)
 
+        if use_pandas:
+            import pandas
+            return pandas.to_pickle(x, path, **kwargs)
+
+        import pickle
         with open(path, "wb") as fd:
-            return pickle.dump(x, fd)
+            return pickle.dump(x, fd, **kwargs)
+
+    def write_params(self):
+        return {
+            "pandas": bool,
+        }
