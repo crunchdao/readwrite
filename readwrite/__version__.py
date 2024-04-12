6792f7f6ac9367f4d392496c438033efa5166ec7
Caceresenzo <caceresenzo1502@gmail.com>
2024-04-12 14:42:48 +0200

    release: bump to 0.7.0

--- readwrite/__version__.py
+++ readwrite/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'readwrite'
 __description__ = 'readwrite - Quickly read/write file in common formats'
-__version__ = '0.6.0'
+__version__ = '0.7.0'
 __author__ = 'Enzo CACERES'
 __author_email__ = 'enzo.caceres@crunchdao.com'
 __url__ = 'https://github.com/crunchdao/readwrite'
